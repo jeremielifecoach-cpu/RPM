7eef879a893dbb8343c98c284115760f70c45571
jeremielifecoach-cpu <jeremielifecoach@gmail.com>
2026-09-12 10:25:15 +0200

    Update page.tsx

--- src/app/domaines/page.tsx
+++ src/app/domaines/page.tsx
@@ -1,10 +1,10 @@
+export const dynamic = "force-dynamic";
+
 import { ensureAreasSeeded, getAreas } from "@/lib/data";
 import { AreasClient } from "@/components/areas-client";
-
-export const dynamic = "force-dynamic";
 
 export default async function DomainesPage() {
   await ensureAreasSeeded();
   const areas = await getAreas();
-  return <AreasClient areas={areas} />;
+  return <AreasClient initialAreas={areas} />;
 }
